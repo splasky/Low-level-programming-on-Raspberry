2884641ae58fea57e0e7dd65e01cdd4dd683baf9
Splasky <henry860326@yahoo.com.tw>
2016-06-17 14:03:35 +0800

    remove debug code

--- src/ADXL345.c
+++ src/ADXL345.c
@@ -2,6 +2,8 @@
 
 void ADXL345_Init(void)
 {
+    BSC0_S = CLEAR_STATUS;	// Reset status bits (see #define)
+    BSC0_C = BSC_C_CLEAR; 
     ADXL345_SetRegister(DATA_FORMAT,DATA_FORMAT_DATA);
     ADXL345_SetRegister(POWER_CTL,POWER_CTL_DATA);
 }
@@ -23,7 +25,7 @@ void ADXL345_SetRegister(unsigned const char regAddr,unsigned const char regValu
 void ADXL345_Read(short *accData)
 {
     BSC0_DLEN = 1;    		// one byte
-    BSC0_FIFO = 0x00;    	// value of first register
+    BSC0_FIFO = 0x32;    	// value of first register
     BSC0_S = CLEAR_STATUS; 	// Reset status bits (see #define)
     BSC0_C = START_WRITE;   // Start Write (see #define)
 
@@ -35,19 +37,19 @@ void ADXL345_Read(short *accData)
     BSC0_C = START_READ;   	// Start Read after clearing FIFO (see #define)
 
     wait_i2c_done();
-    printf("initial:%d",BSC0_FIFO = 0x00);
-    printf("x0:%d\t",BSC0_FIFO = 0x32);
-    printf("x1:%d\n",BSC0_FIFO = 0x33);
-    printf("y0:%d\t",BSC0_FIFO = 0x34);
-    printf("y1:%d\n",BSC0_FIFO = 0x35);
-    printf("z0:%d\t",BSC0_FIFO = 0x36);
-    printf("z1:%d\n",BSC0_FIFO = 0x37);
+    /*printf("initial:%x\n",BSC0_FIFO);*/
     /*
-     *short tmp;
-	 *int tmpAddr[]={0x32,0x33,0x34,0x35,0x36,0x37};
-     *for(int i=0; i < 3; i++)// Accelerometer
-     *{
-	 *accData[i] = ((BSC0_FIFO = tmpAddr[1+i*2]) << 8 &0xFF00) |(BSC0_FIFO = tmpAddr[0+i*2] & 0xFF) ;	
-     *}
+     *printf("x0:%x\t",(BSC0_FIFO & 0xFF));
+     *printf("x1:%x\n",(BSC0_FIFO << 8) & 0xFF00) ;
+     *printf("y0:%x\t",(BSC0_FIFO & 0xFF));
+     *printf("y1:%x\n",(BSC0_FIFO << 8) & 0xFF00) ;
+     *printf("z0:%x\t",(BSC0_FIFO & 0xFF));
+     *printf("z1:%x\n",(BSC0_FIFO << 8) & 0xFF00) ;
      */
+    accData[0] = (((BSC0_FIFO << 8) & 0xFF00) |(BSC0_FIFO & 0xFF));	
+    accData[1] = (((BSC0_FIFO << 8) & 0xFF00) |(BSC0_FIFO & 0xFF));	
+    accData[2] = (((BSC0_FIFO << 8) & 0xFF00) |(BSC0_FIFO & 0xFF));	
+    
+    BSC0_C = BSC_C_CLEAR; 
+    wait_i2c_done();
 }
